4f67c3486a59dd7c6f4b701563a7bd4f18687d1b
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-04 02:52:35 +0100

    created pages & added scss stylesheets

---
+++ src/pages/New.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function New() {
+    return (
+        <div className="new">
+            <h1>New</h1>
+        </div>
+    )
+}
+
+export default New;
---
+++ src/pages/Single.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Single() {
+    return (
+        <div className="single">
+            <h1>Single</h1>
+        </div>
+    )
+}
+
+export default Single;
