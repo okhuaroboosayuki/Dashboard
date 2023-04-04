5db3814d52122ab6749e2a7f939f85997b4ef584
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-04 14:19:28 +0100

    added routes to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './assets/css/style.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/css/style.css";
+import App from "./App";
+import Login from "./pages/Login";
+import List from "./pages/List";
+import Single from "./pages/Single";
+import New from "./pages/New";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "users",
+        element: <List />,
+        children: [
+          {
+            path: ":userId",
+            element: <Single />
+          },
+          {
+            path: "new",
+            element: <New />
+          }
+        ],
+      },
+      {
+        path: "products",
+        element: <List />,
+        children: [
+          {
+            path: ":productId",
+            element: <Single />,
+          },
+          {
+            path: "new",
+            element: <New />
+          },
+        ],
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
