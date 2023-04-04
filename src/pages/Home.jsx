65544fad10e9486485347b3615083768297f667d
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-04 14:20:26 +0100

    imported stylesheet, & navigation components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import SideBar from "../components/SideBar";
+import "../assets/scss/home.scss";
+import NavBar from "../components/NavBar";
 
 function Home() {
-    return (
-        <div className="home">
-            <h1>Home</h1>
-        </div>
-    )
+  return (
+    <div className="home">
+      <SideBar />
+      <div className="home_container">
+        <NavBar />
+        container
+      </div>
+    </div>
+  );
 }
 
 export default Home;
