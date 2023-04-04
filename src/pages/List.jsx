340eba6d9aa8c3295edd021aff08429b0ac202d0
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-04 14:21:09 +0100

    added an outlet for children routes

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 function List() {
     return (
         <div className="list">
             <h1>List</h1>
+            <Outlet />
         </div>
     )
 }
