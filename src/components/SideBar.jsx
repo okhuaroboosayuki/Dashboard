54ecffd33481a77dbef724f9ef765054d59d506f
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-04 14:18:40 +0100

    imported icons from material ui icons & added side navigation content

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,31 +1,96 @@
 import "../assets/scss/sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonIcon from "@mui/icons-material/Person";
+import StoreIcon from "@mui/icons-material/Store";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import AnalyticsIcon from "@mui/icons-material/Analytics";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MedicalInformationIcon from "@mui/icons-material/MedicalInformation";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 function SideBar() {
-    return (
-        <section className="side_bar">
-            <div className="top">
-                <span className="logo">Admin</span>
-            </div>
-            <hr />
-            <div className="center">
-                <ul>
-                    <li>
-                        <span>Dashboard</span>
-                    </li>
-                    <li>
-                        <span>Dashboard</span>
-                    </li>
-                    <li>
-                        <span>Dashboard</span>
-                    </li>
-                    <li>
-                        <span>Dashboard</span>
-                    </li>
-                </ul>
-            </div>
-            <div className="bottom">color option</div>
-        </section>
-    )
+  return (
+    <section className="side_bar">
+      <div className="top">
+        <span className="logo">Admin</span>
+      </div>
+
+      <hr />
+
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+
+          <p className="title">LISTS</p>
+          <li>
+            <PersonIcon className="icon" />
+            <span>Users</span>
+          </li>
+          <li>
+            <StoreIcon className="icon" />
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+
+          <p className="title">USEFUL</p>
+          <li>
+            <AnalyticsIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+
+          <p className="title">SERVICE</p>
+          <li>
+            <MedicalInformationIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+
+      <hr />
+      
+      <div className="bottom">
+        <div className="color_option"></div>
+        <div className="color_option"></div>
+      </div>
+    </section>
+  );
 }
 
 export default SideBar;
