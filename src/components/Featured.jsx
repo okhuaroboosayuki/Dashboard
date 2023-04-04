14442da0cb9d26fd1eca7015e5e3b03f01790511
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-04 21:48:31 +0100

    added content

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,5 +1,9 @@
 import "../assets/scss/featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 function Featured() {
   return (
@@ -9,7 +13,43 @@ function Featured() {
         <MoreVertIcon fontSize="small" />
       </div>
 
-      <div className="bottom"></div>
+      <div className="bottom">
+        <div className="featured_chart">
+          <CircularProgressbar value={80} text={"80%"} strokeWidth={4} />
+        </div>
+
+        <p className="title">Total sales made today</p>
+        <p className="amount">$500</p>
+        <p className="desc">
+          Previous transactions processing. Last payments may not be included.
+        </p>
+
+        <div className="summary">
+          <div className="item">
+            <div className="item_title">Target</div>
+            <div className="item_result positive">
+              <ArrowDropUpIcon />
+              <div className="result_amount">$15.3k</div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="item_title">Last Week</div>
+            <div className="item_result negative">
+              <ArrowDropDownIcon />
+              <div className="result_amount">$10.8k</div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="item_title">Last Month</div>
+            <div className="item_result positive">
+              <ArrowDropUpIcon />
+              <div className="result_amount">$12.3k</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
