f1ec0d3358b3f896996ccec1ffee28b04f23d942
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-06 00:14:19 +0100

    used app as a routing point

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { Outlet } from "react-router-dom";
+import Login from "./pages/Login";
+import Single from "./pages/Single";
+import New from "./pages/New";
+import List from "./pages/List";
 import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Outlet />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
