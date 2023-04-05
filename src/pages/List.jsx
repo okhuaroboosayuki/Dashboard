80e239af6f60d68612d59daa4f9266e2f8985c2b
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-06 00:15:32 +0100

    added content & imported DataTable component

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import "../assets/scss/list.scss";
+import Sidebar from "../components/Sidebar";
+import NavBar from "../components/NavBar";
+import DataTable from "../components/DataTable";
 
 function List() {
-    return (
-        <div className="list">
-            <h1>List</h1>
-            <Outlet />
-        </div>
-    )
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="list_container">
+        <NavBar />
+        <DataTable />
+      </div>
+    </div>
+  );
 }
 
 export default List;
