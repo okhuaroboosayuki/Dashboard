06826ca9cb4cb075690449777c466bea8b6886ce
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-06 21:40:44 +0100

    implemented darkMode functionalities & removed some icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,15 @@
 import "../assets/scss/navbar.scss";
 import SearchIcon from "@mui/icons-material/Search";
-import LanguageIcon from '@mui/icons-material/Language';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import ListIcon from '@mui/icons-material/List';
-
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import { DarkModeContext } from "../context/darkModeContext";
+import { useContext } from "react";
 
 function NavBar() {
+  const { darkMode, dispatch } = useContext(DarkModeContext);
+
   return (
     <nav className="nav_bar">
       <div className="wrapper">
@@ -18,31 +19,38 @@ function NavBar() {
         </div>
 
         <div className="items">
-            <div className="item">
-                <LanguageIcon className="icon" />
-                English
-            </div>
-            <div className="item">
-                <DarkModeIcon className="icon" />
-            </div>
-            <div className="item">
-                <FullscreenExitIcon className="icon" />
-            </div>
-            <div className="item">
-                <NotificationsNoneIcon className="icon" />
-                <div className="counter">1</div>
-            </div>
-            <div className="item">
-                <ChatBubbleOutlineIcon className="icon" />
-                <div className="counter">2</div>
-            </div>
-            <div className="item">
-                <ListIcon className="icon" />
-            </div>
-            
-            <div className="item">
-                <img src="https://res.cloudinary.com/dg0sck16v/image/upload/v1680613170/Avatar/charlesdeluvio-K4mSJ7kc0As-unsplash_lbldzv.jpg" alt="avatar" className="avatar" />
-            </div>
+          <div className="item">
+            {/* <DarkModeIcon
+              className="icon"
+              onClick={() => dispatch({ type: "TOGGLE_MODE" })}
+            /> */}
+            {darkMode ? (
+              <LightModeIcon
+                className="icon"
+                onClick={() => dispatch({ type: "TOGGLE_MODE" })}
+              />
+            ) : (
+              <DarkModeIcon
+                className="icon"
+                onClick={() => dispatch({ type: "TOGGLE_MODE" })}
+              />
+            )}
+          </div>
+          <div className="item">
+            <NotificationsNoneIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineIcon className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <img
+              src="https://res.cloudinary.com/dg0sck16v/image/upload/v1680613170/Avatar/charlesdeluvio-K4mSJ7kc0As-unsplash_lbldzv.jpg"
+              alt="avatar"
+              className="avatar"
+            />
+          </div>
         </div>
       </div>
     </nav>
