f7b30cd5c6096c2e69bef58a0c7110188f53646b
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-06 05:27:36 +0100

    creeated data for form inputs

---
+++ src/formData.js
@@ -0,0 +1,76 @@
+export const userInputs = [
+    {
+      id: 1,
+      label: "Username",
+      type: "text",
+      placeholder: "Paulo",
+    },
+    {
+      id: 2,
+      label: "Name and surname",
+      type: "text",
+      placeholder: "Paul Scholes",
+    },
+    {
+      id: 3,
+      label: "Email",
+      type: "mail",
+      placeholder: "paul@mail.com",
+    },
+    {
+      id: 4,
+      label: "Phone",
+      type: "text",
+      placeholder: "+123...",
+    },
+    {
+      id: 5,
+      label: "Password",
+      type: "password",
+    },
+    {
+      id: 6,
+      label: "Address",
+      type: "text",
+      placeholder: "my street, my city",
+    },
+    {
+      id: 7,
+      label: "Country",
+      type: "text",
+      placeholder: "my country",
+    },
+  ];
+  
+  export const productInputs = [
+    {
+      id: 1,
+      label: "Title",
+      type: "text",
+      placeholder: "Apple Macbook Pro",
+    },
+    {
+      id: 2,
+      label: "Description",
+      type: "text",
+      placeholder: "Description",
+    },
+    {
+      id: 3,
+      label: "Category",
+      type: "text",
+      placeholder: "Computers",
+    },
+    {
+      id: 4,
+      label: "Price",
+      type: "text",
+      placeholder: "100",
+    },
+    {
+      id: 5,
+      label: "Stock",
+      type: "text",
+      placeholder: "in stock",
+    },
+  ];
