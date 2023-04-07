eb3d1f458d3f14d300f060a15452787e248b5715
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-07 23:51:08 +0100

    added ProgressProvider to control bar animation, & tweaked the icons of user added data

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -2,47 +2,68 @@ import "../assets/scss/featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import { useEffect, useState } from "react";
 
 function Featured() {
+  const ProgressProvider = ({ valueStart, valueEnd, children }) => {
+    const [value, setValue] = useState(valueStart);
+    useEffect(() => {
+      setValue(valueEnd);
+    }, [valueEnd]);
+
+    return children(value);
+  };
+
+  const [valueEnd, setValueEnd] = useState(0);
+
+  useEffect(() => {
+    setValueEnd(Math.round(8.56));
+  }, []);
+
   return (
     <div className="featured">
       <div className="top">
-        <h1 className="title">Total Users</h1>
+        <h1 className="title">Total Users Added</h1>
         <MoreVertIcon fontSize="small" />
       </div>
 
       <div className="bottom">
-        <div className="featured_chart">
-          <CircularProgressbar value={80} text={"80%"} strokeWidth={4} />
+        <div className="featured_chart rotate_animation">
+          <ProgressProvider valueStart={0} valueEnd={valueEnd} >
+            {value => (
+              <CircularProgressbar value={value} text={`${value}%`} strokeWidth={4} />
+            )}
+          </ProgressProvider>
         </div>
 
         <p className="title">Total users added today</p>
-        <p className="amount">1000</p>
+        <p className="amount">1284</p>
 
         <div className="summary">
           <div className="item">
             <div className="item_title">Target</div>
-            <div className="item_result positive">
-              <ArrowDropUpIcon />
+            <div className="item_result negative">
+              <ArrowDropDownIcon />
               <div className="result_amount">15k</div>
             </div>
           </div>
 
           <div className="item">
             <div className="item_title">Last Week</div>
-            <div className="item_result negative">
-              <ArrowDropDownIcon />
+            <div className="item_result positive">
+              <ArrowDropUpIcon />
               <div className="result_amount">800</div>
             </div>
           </div>
 
           <div className="item">
             <div className="item_title">Last Month</div>
-            <div className="item_result positive">
-              <ArrowDropUpIcon />
-              <div className="result_amount">12.3k</div>
+            <div className="item_result neutral">
+              <ArrowRightIcon />
+              <div className="result_amount">3k</div>
             </div>
           </div>
         </div>
