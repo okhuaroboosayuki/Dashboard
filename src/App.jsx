43a3f5310293f97a8d924ff10d4b8ec3430a6692
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-20 16:50:19 +0100

    changed route props, & removed productInputs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import New from "./pages/New";
 import List from "./pages/List";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { productInputs, userInputs } from "./formData";
+import { userInputs } from "./formData";
 import "./assets/scss/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
@@ -18,14 +18,14 @@ function App() {
           <Route path="/">
             <Route index element={<Home />} />
             <Route path="users">
-              <Route index element={<List />} />
+              <Route index element={<List title="Add New User" />} />
               <Route path=":userId" element={<Single />} />
               <Route path="new" element={<New inputs={userInputs} title="Add New User" />} />
             </Route>
-            <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New inputs={productInputs} title="Add New Product" />} />
+            <Route path="Units">
+              <Route index element={<List title="Add New Unit" />} />
+              <Route path=":unitId" element={<Single />} />
+              <Route path="new" element={<New inputs={userInputs} title="Add New Unit" />} />
             </Route>
           </Route>
         </Routes>
