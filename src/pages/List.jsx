e4d37c704275897e31e9c6a6135d86a9300e9198
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-04-20 16:59:02 +0100

    called props from parent & added props title to DataTable component

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -3,13 +3,16 @@ import SideBar from "../components/SideBar"
 import NavBar from "../components/NavBar";
 import DataTable from "../components/DataTable";
 
-function List() {
+function List(props) {
   return (
     <div className="list">
       <SideBar />
       <div className="list_container">
         <NavBar />
-        <DataTable />
+        {/* <DataTable /> */}
+
+        {/* // pass props to DataTable */}
+        <DataTable title={props.title} />
       </div>
     </div>
   );
